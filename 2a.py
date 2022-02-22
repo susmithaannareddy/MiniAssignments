2384686e973e5884b314237d88e71451c6f56a6b
asusmitha <ASUSMITHA@DELOITTE.COM>
2022-02-22 16:00:34 +0530

    2(a) Assignment

--- 2a.py
+++
@@ -1,6 +0,0 @@
-#2(a) Diamond pattren
-h = eval(input("Enter diamond's height: "))
-for x in range(h):
-    print(" " * (h - x), "*" * (2*x + 1))
-for x in range(h - 2, -1, -1):
-    print(" " * (h - x), "*" * (2*x + 1))
